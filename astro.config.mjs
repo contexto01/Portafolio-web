67e18bacb9a6414450fdc01237ec2305183b2d04
Daniel Barrera <daniel.barrera02121@hotmail.com>
2024-11-04 13:45:51 -0500

    Add roboto.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
+import robotsTxt from 'astro-robots-txt'
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://contextoportfolio.netlify.app/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), robotsTxt()]
 })
